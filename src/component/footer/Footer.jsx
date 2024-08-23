6c8b2eb07a54a901e307bcb1f17447f817eff6c4
AdelAhmed2000 <adelahmedsayed399@gmail.com>
2024-08-23 19:18:44 +0300

    new

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../../style/component/MainFooter/MainFooter.css";
 import { SecondFooter } from "./SecondFooter";
 import { CobyRight } from "./CobyRight";
-import { FirstFooter } from "./FirstFooter";
+import { FooterTwo } from "./FooterTwo";
 
 export const Footer = () => {
   return (
     <>
       <div className="Main_footer">
         <div className="container">
-          <FirstFooter />
+          <FooterTwo />
           <SecondFooter />
           <CobyRight />
         </div>
